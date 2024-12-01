7f8d2608f1c2674a06b61b6ca24762b400b8d293
William Hemmingsson <william.hemmingsson@gmail.com>
2024-12-01 16:28:43 +0100

    Day 1 - cleanup

--- 1/index.js
+++ 1/index.js
@@ -1,34 +1,20 @@
 const Parser = require("../common/parser.js");
 module.exports = class Day {
     static run() {
-        const data = Parser.readRaw(__dirname, false).split("\n");
-
-        let leftOrg = [];
-        let rightOrg = [];
-        data.forEach(line => {
+        const left = [];
+        const right = [];
+        Parser.readRaw(__dirname, false).split("\n").forEach(line => {
             const r = line.match(/\d+/g);
-            leftOrg.push(parseInt(r[0]));
-            rightOrg.push(parseInt(r[1]));
+            left.push(parseInt(r[0]));
+            right.push(parseInt(r[1]));
         });
 
         // Part 1
-        let left = [...leftOrg];
-        let right = [...rightOrg];
-        let distSum = 0;
-        while (left.length > 0) {
-            const smallestLeft = Math.min(...left);
-            const smallestRight = Math.min(...right);
-            distSum += Math.abs(smallestLeft - smallestRight);
-            left.splice(left.indexOf(smallestLeft, 0), 1);
-            right.splice(right.indexOf(smallestRight, 0), 1);
-        }
-
-        console.log("Part 1", distSum);
+        left.sort((a, b) => a - b);
+        right.sort((a, b) => a - b);
+        console.log("Part 1", left.reduce((s, l, i) => s + Math.abs(l - right[i]), 0));
 
         // Part 2
-        left = [...leftOrg];
-        right = [...rightOrg];
-        distSum = left.reduce((s, l) => s + (right.filter(r => r === l).length * l), 0);
-        console.log("Part 2", distSum);
+        console.log("Part 2", left.reduce((s, l) => s + (right.filter(r => r === l).length * l), 0));
     };
 }
