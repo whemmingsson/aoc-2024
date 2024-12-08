20e3d8f85f4e697d16658cb13b260351ab9445eb
William Hemmingsson <william.hemmingsson@gmail.com>
2024-12-09 00:11:20 +0100

    Day 8 - Cleaned

--- 8/index.js
+++ 8/index.js
@@ -2,72 +2,57 @@ const Parser = require("../common/parser.js");
 module.exports = class Day {
     static run() {
         const map = [];
-        const antennas = {};
+        const a = {};
         const regex = /[a-z]|[A-Z]|\d/;
         Parser.readRaw(__dirname, false).split("\n").forEach((r, ri) => {
             map[ri] = [];
             r.trim().split("").forEach((v, ci) => {
                 map[ri].push(v);
                 if (!regex.test(v)) return;
-                if (!antennas[v]) {
-                    antennas[v] = [];
+                if (!a[v]) {
+                    a[v] = [];
                 }
-                antennas[v].push({ r: ri, c: ci });
+                a[v].push({ r: ri, c: ci });
             });
         });
 
-        console.log(antennas);
-        const w = map.length;
-        console.log(w);
-
-        const constructAntinodes = (a, b) => {
-            const d = { r: b.r - a.r, c: b.c - a.c }
+        const countAntinodes = (a, b) => {
+            let count = 0;
+            const d = { r: b.r - a.r, c: b.c - a.c };
             const inBounds = (r, c) => c < map.length && r < map[0].length && c >= 0 && r >= 0;
+            const countUpdate = (dr, dc) => {
+                if (map[r][c] !== "#") {
+                    count++;
+                    map[r][c] = "#";
+                }
+                c += dc;
+                r += dr;
+            }
 
-            const antinodes = [];
             let c = a.c, r = a.r;
             while (inBounds(r, c)) {
-                if (!foundAntiNodes[key({ r, c })]) antinodes.push({ r, c });
-                c += d.c;
-                r += d.r;
+                countUpdate(d.r, d.c);
             }
 
             c = a.c;
             r = a.r;
             while (inBounds(r, c)) {
-                const k = key({ r, c });
-                if (!foundAntiNodes[key({ r, c })]) antinodes.push({ r, c });
-                c -= d.c;
-                r -= d.r;
+                countUpdate(-d.r, -d.c);
             }
-            return antinodes;
+
+            return count;
         }
 
-        let totalAntiNodes = 0;
-        let sneakyCount = 0;
-        let foundAntiNodes = {};
-        let key = (pos) => pos.r + "_" + pos.c;
-        Object.keys(antennas).forEach(k => {
-            for (let i = 0; i < antennas[k].length; i++) {
-                for (let j = i + 1; j < antennas[k].length; j++) {
-                    const nodes = constructAntinodes(antennas[k][i], antennas[k][j]);
-                    for (let n = 0; n < nodes.length; n++) {
-                        let antinode = nodes[n];
-                        const nk = key(antinode);
-                        console.log(antinode, map[antinode.r][antinode.c]);
-                        if (!foundAntiNodes[nk]) {
-                            foundAntiNodes[nk] = true;
-                            totalAntiNodes++;
-                        }
-                        else {
-                            console.log("    Sneaky", antinode, map[antinode.r][antinode.c]);
-                            sneakyCount++;
-                        }
-                    }
+        const count = (k) => {
+            let sum = 0;
+            for (let i = 0; i < a[k].length; i++) {
+                for (let j = i + 1; j < a[k].length; j++) {
+                    sum += countAntinodes(a[k][i], a[k][j]);
                 }
             }
-        });
+            return sum;
+        }
 
-        console.log("Result", totalAntiNodes, sneakyCount);
+        console.log("Result", Object.keys(a).map(count).reduce((a, c) => a + c, 0));
     }
 }
