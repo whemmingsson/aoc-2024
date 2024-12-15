0288ec6635479a80e2bb5d9a79b100d817a93a31
William Hemmingsson <william.hemmingsson@gmail.com>
2024-12-15 23:26:07 +0100

    Day 15 - Cleaning and adjusting rendering

--- 15/vis/index.js
+++ 15/vis/index.js
@@ -1,7 +1,8 @@
-const useExample = false;
+const useExample = false; // Use simpler data
+const render = true; // If false, the algoritm runs until completion without rendering
 
 // Display variables
-const cellSize = 25;
+const cellSize = 15;
 const speed = 10;
 
 // Algoritm variables
@@ -35,6 +36,9 @@ const createMap = () => {
                 row.push("]");
             }
             else if (c === "@") {
+                robotPos = {
+                    x: x * 2, y
+                }
                 row.push("@");
                 row.push(".");
             }
@@ -56,25 +60,12 @@ const createInstructions = () => {
     return instructions;
 }
 
-const getStartPosition = () => {
-    for (let y = 0; y < size.h; y++) {
-        for (let x = 0; x < size.w; x++) {
-            if (map[y][x] === "@") return { x, y };
-        }
-    }
-}
-
 const createDirectionsMap = () => {
     let m = {};
     cardinalVectors.forEach(v => {
         m[v.d] = v;
     });
     return m;
-}
-
-const getTargetCell = (instr) => {
-    const vector = directionMap[instr];
-    return map[robotPos.y + vector.y][robotPos.x + vector.x];
 }
 
 const canMoveSimpleCase = (instr) => {
@@ -89,17 +80,9 @@ const canMoveSimpleCase = (instr) => {
     return undefined;
 }
 
-const updateRobotPos = (instr) => {
-    const vector = directionMap[instr];
-    const newPos = { x: robotPos.x += vector.x, y: robotPos.y += vector.y };
-    robotPos.x = newPos.x;
-    robotPos.y = newPos.y;
-}
-
 const moveRobot = (instr) => {
-    const vector = directionMap[instr];
     const originalPos = { x: robotPos.x, y: robotPos.y };
-    const newPos = { x: robotPos.x += vector.x, y: robotPos.y += vector.y };
+    const newPos = { x: robotPos.x += directionMap[instr].x, y: robotPos.y += directionMap[instr].y };
     robotPos.x = newPos.x;
     robotPos.y = newPos.y;
     map[newPos.y][newPos.x] = "@";
@@ -113,7 +96,12 @@ const executeInstruction = () => {
         moveRobot(instruction);
     }
     else if (!(simpleMove !== undefined && !simpleMove)) {
-        handleComplexMove(instruction);
+        if (instruction === ">" || instruction === "<") {
+            handleHorizontalMove(instruction);
+        }
+        else {
+            handleVerticalMove(instruction);
+        }
     }
 
     instructionIndex++;
@@ -123,10 +111,17 @@ const isBoxAt = (p) => {
     return map[p.y][p.x] === "[" || map[p.y][p.x] === "]";
 }
 
+const isWallAt = (p) => {
+    return map[p.y][p.x] === "#";
+}
+
+let keyOf = (p) => {
+    return p.x + "_" + p.y;
+}
+
 const handleHorizontalMove = (instr) => {
     const vector = directionMap[instr];
-    const originalPos = { x: robotPos.x, y: robotPos.y };
-    let boxPos = { x: originalPos.x + vector.x, y: originalPos.y + vector.y }
+    let boxPos = { x: robotPos.x + vector.x, y: robotPos.y + vector.y }
 
     let cellsToShift = [];
     cellsToShift.push({ ...robotPos });
@@ -136,7 +131,7 @@ const handleHorizontalMove = (instr) => {
         boxPos.y += vector.y;
     };
 
-    if (map[boxPos.y][boxPos.x] === "#") {
+    if (isWallAt(boxPos)) {
         return;
     }
 
@@ -148,74 +143,46 @@ const handleHorizontalMove = (instr) => {
     moveRobot(instr);
 }
 
-const constructFullBox = (p) => {
-    let keyOf = (p) => {
-        return p.x + "_" + p.y;
-    }
-
+const createWideBox = (p) => {
     let p1 = { ...p, v: map[p.y][p.x] };
     let p2 = map[p.y][p.x] === "]" ? { x: p.x - 1, y: p.y, v: "[" } : { x: p.x + 1, y: p.y, v: "]" }
-    const smallestX = Math.min(p1.x, p2.x);
-    return { p1, p2, key: keyOf({ x: smallestX, y: p.y }), y: p.y, x: smallestX };
+    return { p1, p2, y: p.y };
 }
 
-const findAllBoxesAbove = (box, boxes, found, vecY) => {
-    const test1 = { x: box.p1.x, y: box.y + vecY };
+const findBoxes = (box, boxes, vecY) => {
     const nextBoxes = [];
+    const test1 = { x: box.p1.x, y: box.y + vecY };
+
     if (isBoxAt(test1)) {
-        let foundBox = constructFullBox(test1);
-        let k = foundBox.key;
-        if (!found[k]) {
-            found[k] = foundBox;
-            nextBoxes.push(foundBox);
-        }
+        nextBoxes.push(createWideBox(test1));
     }
 
     const test2 = { x: box.p2.x, y: box.y + vecY };
     if (isBoxAt(test2)) {
-        let foundBox = constructFullBox(test2);
-        let k = foundBox.key;
-        if (!found[k]) {
-            found[k] = foundBox;
-            nextBoxes.push(foundBox);
-        }
+        nextBoxes.push(createWideBox(test2));
     }
 
-    if (nextBoxes.length > 0) {
-        boxes.push(...nextBoxes);
-
-        nextBoxes.forEach(b => {
-            findAllBoxesAbove(b, boxes, found, vecY);
-        })
-    }
+    boxes.push(...nextBoxes);
+    nextBoxes.forEach(b => {
+        findBoxes(b, boxes, vecY);
+    });
 }
 
-const findAllBoxesAux = (box, vecY) => {
-    let found = {};
-    let allBoxes = [];
-    found[box.key] = box;
-    allBoxes.push(box);
-    findAllBoxesAbove(box, allBoxes, found, vecY);
+const findBoxesAux = (box, vecY) => {
+    let allBoxes = [box];
+    findBoxes(box, allBoxes, vecY);
     return allBoxes;
 }
 
 const handleVerticalMove = (instr) => {
     const vector = directionMap[instr];
-    const originalPos = { x: robotPos.x, y: robotPos.y };
+    const allBoxes = findBoxesAux(createWideBox({ x: robotPos.x + vector.x, y: robotPos.y + vector.y }), vector.y);
 
-    let allBoxes = [];
-    let box1 = constructFullBox({ x: originalPos.x + vector.x, y: originalPos.y + vector.y })
-    allBoxes = findAllBoxesAux(box1, vector.y);
-
-    const canMove = allBoxes.every(b => map[b.y + vector.y][b.p1.x] !== "#" && map[b.y + vector.y][b.p2.x] !== "#");
-
-    if (!canMove) {
+    if (!allBoxes.every(b => !isWallAt({ x: b.p1.x, y: b.y + vector.y }) && !isWallAt({ x: b.p2.x, y: b.y + vector.y }))) {
         return;
     }
 
-    allBoxes.sort((a, b) => vector.y === -1 ? a.y - b.y : b.y - a.y);
-
-    allBoxes.forEach(b => {
+    allBoxes.sort((a, b) => vector.y === -1 ? a.y - b.y : b.y - a.y).forEach(b => {
         map[b.p1.y][b.p1.x] = "."
         map[b.p1.y + vector.y][b.p1.x] = b.p1.v;
         map[b.p2.y][b.p2.x] = "."
@@ -223,16 +190,6 @@ const handleVerticalMove = (instr) => {
     })
 
     moveRobot(instr);
-}
-
-const handleComplexMove = (instr) => {
-    if (instr === ">" || instr === "<") {
-        handleHorizontalMove(instr);
-    }
-    else {
-        // Vertical move. A bit more tricky. 
-        handleVerticalMove(instr);
-    }
 }
 
 const calculateGPSDistance = () => {
@@ -247,39 +204,54 @@ const calculateGPSDistance = () => {
     return sum;
 }
 
-function setup() {
-    // Render setup
-    createCanvas(2700, 1400);
-    textAlign(CENTER, CENTER);
-    textSize(cellSize * 0.6);
+const speedCalc = (s) => {
+    for (let i = 0; i < s; i++) {
+        executeInstruction();
+        if (instructionIndex >= instructions.length) {
+            noLoop();
+            console.log("GPS sum:", calculateGPSDistance())
+            break;
+        }
+    }
+}
 
+function setup() {
     // Data setup
     map = createMap();
     instructions = createInstructions();
     size = { h: map.length, w: map[0].length }
-    robotPos = getStartPosition();
     directionMap = createDirectionsMap();
+
+    if (!render) {
+        noLoop();
+        speedCalc(instructions.length);
+        createCanvas(5, 5);
+    }
+    else {
+        createCanvas(2700, 1400);
+        textAlign(CENTER, CENTER);
+        textSize(cellSize * 0.6);
+    }
 }
 
 const drawCell = (v, x, y) => {
     if (v === "#") {
-        fill(150, 50, 50);
+        fill(20, 10, 20);
         rect(x, y, cellSize, cellSize);
     }
     else if (v === "@") {
         noStroke();
-        fill(0, 100, 250);
+        fill(255, 255, 255, 100);
         rect(x, y, cellSize, cellSize);
     }
     else if (v === ".") {
-        //stroke(0);
-        fill(20);
-        rect(x, y, cellSize, cellSize);
+        //fill(40, 70, 40);
+        //rect(x, y, cellSize, cellSize);
     }
     else if (v === "[") {
-        stroke(150, 106, 3);
+        stroke(150, 106, 3, 50);
         strokeWeight(2);
-        fill(173, 122, 3);
+        fill(173, 122, 3, 50);
         rect(x + 2, y + 2, (cellSize - 2) * 2, cellSize - 4);
         noStroke();
     }
@@ -291,41 +263,10 @@ const drawMap = () => {
             drawCell(map[y][x], x * cellSize, y * cellSize)
         }
     }
-
-    /*// Display shade of where the bot should move next 
-    if (instructionIndex < instructions.length - 1) {
-        const vec = directionMap[instructions[instructionIndex]];
-        const rX = robotPos.x + vec.x;
-        const rY = robotPos.y + vec.y;
-        fill(255, 20, 20, 100);
-        rect(rX * cellSize, rY * cellSize, cellSize, cellSize);
-    } */
-}
-
-const speedCalc = () => {
-    for (let i = 0; i < speed; i++) {
-        executeInstruction();
-        if (instructionIndex >= instructions.length) {
-            noLoop();
-            console.log("No more instructions");
-            console.log("GPS sum:", calculateGPSDistance())
-            break;
-        }
-    }
-}
-
-const speedAhead = () => {
-    for (let i = 0; i < 188; i++) {
-        executeInstruction();
-    }
-    drawMap();
 }
 
 function draw() {
-    background(20);
-    speedCalc();
+    // background(20);
+    speedCalc(speed);
     drawMap();
 }
-
-// 4825 - current known lower bound
-// upper bound: 7852
